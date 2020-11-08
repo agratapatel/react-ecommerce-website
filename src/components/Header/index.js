afd1329661ec2df7a254443d7c9c20c79d708492
agratapatel <agrata@iitg.ernet.in>
2020-11-08 22:27:50 +0530

    Now you can login and logout with Google. Data will be stored in Firebase.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,11 @@
 import React  from 'react';
 import './styles.scss';
 import { Link } from 'react-router-dom';
+import { auth } from './../../firebase/utils';
 
 const Header = props => {
+    const { currentUser } = props;
+
     return (
       <header className="header">
         <div className="wrap">
@@ -13,15 +16,35 @@ const Header = props => {
           </div>
 
           <div className="callToActions">
-            <ul>
-              <li>
-                <Link to="/registration">Register</Link>
-              </li>
-            </ul>
+
+            {currentUser && (
+                <ul>
+                    <li>
+                        <span onClick ={() => auth.signOut()}>
+                            Logout
+                        </span>
+                    </li>
+                </ul>
+            )}
+
+            {!currentUser && (
+              <ul>
+                <li>
+                  <Link to="/registration">Register</Link>
+                </li>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+              </ul>
+            )}
           </div>
         </div>
       </header>
     );
-}
+};
+
+Header.defaultProps = {
+    currentUser: null
+};
 
 export default Header;
---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+
+    apiKey: "AIzaSyAotBP484QzRdT_QoWml9cVt5U-4gXRzN8",
+    authDomain: "e-commerce-website-c1a41.firebaseapp.com",
+    databaseURL: "https://e-commerce-website-c1a41.firebaseio.com",
+    projectId: "e-commerce-website-c1a41",
+    storageBucket: "e-commerce-website-c1a41.appspot.com",
+    messagingSenderId: "485979100897",
+    appId: "1:485979100897:web:357cf81ac6f8e4bef009a0",
+
+};
+
